d8a1bcbe9a350f8dc44782e23a53bf6c78c9da6a
NotRobot <evgenijsyncikov7@gmail.com>
2026-06-19 16:30:33 +0300

    guess_number_V2.py

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,6 @@
 import random
 import time
 
-
 def default_game():
         secret = random.randint(1, 100)
         print("Угадай число от", 1, "до", 100)
@@ -31,28 +30,10 @@ def default_game():
                     time.sleep(3)
                     break
 
-print("Выберите режим игры: ")
-time.sleep(0.5)
-print("1. От 1 до 100 | 2. От x до y ")
-time.sleep(0.5)
-a: int = int(input("Введите 1/2: "))
-if a == 1:
-        default_game()
-elif a == 2:
-    while True:
-        x = int(input("От какого числа?: "))
-        time.sleep(0.5)
-        y = int(input("До какого числа?: "))
-        time.sleep(0.5)
-        if x < y:
-            break
-        else:
-            print("Введи числа правильно!")
-            time.sleep(2)
-
+def other_game(x, y):
     secret = random.randint(x, y)
-    time.sleep(0.5)
     print("Угадай число от", x, "до", y)
+    time.sleep(0.5)
     tries = 0
     while True:
         try:
@@ -68,15 +49,28 @@ elif a == 2:
             time.sleep(0.5)
             print("Меньше")
         else:
-             if tries == 1:
+            if tries == 1:
                 time.sleep(0.5)
                 print(f"Угадал с первой попытки!  Это {secret}. Попыток: {tries}")
                 break
-             else:
+            else:
                 time.sleep(0.5)
-                print(f"Угадал! Это {secret}. Попыток: {tries}")
-                time.sleep(3)
-                break
+            print(f"Угадал! Это {secret}. Попыток: {tries}")
+            time.sleep(3)
+            break
+
+
+print("Выберите режим игры: ")
+time.sleep(0.5)
+print("1. От 1 до 100 | 2. От x до y ")
+time.sleep(0.5)
+a: int = int(input("Введите 1/2: "))
+if a == 1:
+        default_game()
+elif a == 2:
+        x = int(input("1 Число?:"))
+        y = int(input("2 Число?:"))
+        other_game(x, y)
 else:
     time.sleep(0.5)
     print("Введи 1/2!")
